d75fbdb9bdfcf58eac9b58a21f373925524b9bd4
adapaushagayathrireddy-dev <adapaushagayathrireddy@gmail.com>
2026-06-04 20:00:32 +0530

    TASK 3
    Uploaded Iris Flower Classification project, source code, and dataset.

---
+++ iris_classification.py
@@ -0,0 +1,33 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load dataset
+iris = load_iris()
+
+X = iris.data
+y = iris.target
+
+# Split data
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+
+# Predictions
+y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", round(accuracy * 100, 2), "%")
+
+# Sample flower prediction
+sample = [[5.1, 3.5, 1.4, 0.2]]
+
+prediction = model.predict(sample)
+
+print("Predicted Flower:", iris.target_names[prediction][0])
